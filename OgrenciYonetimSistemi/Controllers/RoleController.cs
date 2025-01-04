d799ca56d4c80998fe028f6d0bc1888fa6e8a406
Gizem Kayaduman <kayadumangizem@gmail.com>
2025-01-04 22:09:26 +0300

    Proje dosyası ekle.

---
+++ OgrenciYonetimSistemi/Controllers/RoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OgrenciYonetimSistemi.Controllers
+{
+    public class RoleController : Controller
+    {
+        public IActionResult SelecetRole()
+        {
+            return View();
+        }
+    }
+}
---
+++ OgrenciYonetimSistemi/Views/Role/SelecetRole.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OgrenciYonetimSistemi.Views.Role
+{
+    public class SelecetRoleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
